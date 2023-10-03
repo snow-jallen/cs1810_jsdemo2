32082fc711beeb1c6786a88189fc5119b2016124
Jonathan Allen <jallen@snow.edu>
2023-10-03 10:48:01 -0600

    initial files

---
+++ books.js
@@ -0,0 +1,52 @@
+export const bookList = [
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        summary: "Set in the American South during the 1930s, the novel tells the story of Scout Finch and her brother Jem as they navigate issues of race, class, and morality. Their father, Atticus Finch, defends a black man accused of raping a white woman, challenging the deeply ingrained racism of their community."
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        summary: "A dystopian novel set in a totalitarian society where the government, led by Big Brother, exercises total control over people's lives. It explores themes of surveillance, censorship, and the consequences of totalitarianism."
+    },
+    {
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        summary: "A classic novel that tells the story of Elizabeth Bennet and her complicated relationship with the wealthy and aloof Mr. Darcy. The novel explores themes of love, class, and societal expectations in 19th-century England."
+    },
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        summary: "Set in the Roaring Twenties, the novel follows the enigmatic millionaire Jay Gatsby and his obsession with Daisy Buchanan. It delves into themes of wealth, decadence, and the American Dream."
+    },
+    {
+        title: "Moby-Dick",
+        author: "Herman Melville",
+        summary: "A tale of Captain Ahab's relentless pursuit of the white whale, Moby-Dick. The novel explores themes of obsession, revenge, and the inherent struggle between man and nature."
+    },
+    {
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        summary: "Narrated by the disenchanted teenager Holden Caulfield, the novel follows his journey through New York City as he grapples with issues of identity, alienation, and the phoniness of adult society."
+    },
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        summary: "A fantasy novel that tells the adventures of Bilbo Baggins, a hobbit who embarks on a quest to help a group of dwarves reclaim their homeland from the dragon Smaug. It's a prelude to Tolkien's epic, 'The Lord of the Rings.'"
+    },
+    {
+        title: "The Hunger Games",
+        author: "Suzanne Collins",
+        summary: "Set in a dystopian future, the novel follows Katniss Everdeen as she volunteers to participate in the Hunger Games, a televised fight to the death. It explores themes of survival, rebellion, and media manipulation."
+    },
+    {
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        summary: "A philosophical novel that follows Santiago, a shepherd boy, on his journey to find his personal legend and fulfill his dreams. It explores themes of destiny, spirituality, and the pursuit of one's passions."
+    },
+    {
+        title: "The Road",
+        author: "Cormac McCarthy",
+        summary: "A post-apocalyptic novel that follows a father and son as they traverse a desolate and dangerous landscape. It explores themes of survival, love, and the enduring human spirit."
+    }
+];
