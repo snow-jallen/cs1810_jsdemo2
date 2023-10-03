5ea3fb8854070aa85215e22f6217036de2e5dcae
Alex Mickelson <alex.mickelson@snow.edu>
2023-10-03 12:20:20 -0600

    demoed filter and arrays in class

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
-import { bookList } from "./books";
+import { bookList } from "./books.js";
+
+const bookHTML = (book) => `
+<tr>
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.summary}</td>
+</tr>
+`
+
+const tableBodyElement = document.getElementById("myTableBody")
+tableBodyElement.innerHTML = "";
+
+bookList.forEach(book => {
+  const firstBookHtml = bookHTML(book)
+  tableBodyElement.innerHTML += firstBookHtml;
+})
