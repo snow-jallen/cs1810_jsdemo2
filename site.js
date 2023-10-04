3d727f7dedc1a7e87a015a0e3610da3b9699cf17
Alex Mickelson <alex.mickelson@snow.edu>
2023-10-04 12:45:11 -0600

    starting code for lab

--- site.js
+++ site.js
@@ -1,17 +1,22 @@
 import { bookList } from "./books.js";
 
-const bookHTML = (book) => `
-<tr>
-  <td>${book.title}</td>
-  <td>${book.author}</td>
-  <td>${book.summary}</td>
-</tr>
-`
 
-const tableBodyElement = document.getElementById("myTableBody")
-tableBodyElement.innerHTML = "";
+const drawBooks = (books) => {
+  const bookHTML = (book) => `
+    <tr>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.summary}</td>
+    </tr>
+  `
 
-bookList.forEach(book => {
-  const firstBookHtml = bookHTML(book)
-  tableBodyElement.innerHTML += firstBookHtml;
-})
+  const tableBodyElement = document.getElementById("myTableBody")
+  tableBodyElement.innerHTML = "";
+
+  books.forEach(book => {
+    const firstBookHtml = bookHTML(book)
+    tableBodyElement.innerHTML += firstBookHtml;
+  })
+}
+
+drawBooks(bookList);
